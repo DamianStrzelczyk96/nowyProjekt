da0d566ee8dcc1d42d5f14974d22c64774460bde
Your Name <damianstrzelczyk@o2.pl>
2020-03-25 10:22:46 +0100

    Commit 2

---
+++ src/com/company/Kot.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Kot implements Głos {
+
+
+    @Override
+    public String dajGłos() {
+
+        return "miał";
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-
+    Kot kot = new Kot();
+        System.out.println(kot.dajGłos());
     }
 }
